98cfb35b669dfec531260cd9a81a86551f6ce435
Luis Pinilla <luisferpinilla@gmail.com>
2024-07-21 07:40:31 -0500

    Completar lista de parametros en problema

--- bios_utils/problema.py
+++ bios_utils/problema.py
@@ -22,6 +22,7 @@ class Problema():
 
         self.__load_file()
 
+        self.importaciones = self.__load_lista_importaciones()
         self.empresas = self.__load_empresas()
         self.plantas = self.__load_plantas()
         self.ingredientes = self.__load_ingredientes()
@@ -32,7 +33,12 @@ class Problema():
         self.tiempo_disponible, self.tiempo_limpieza, self.tiempo_proceso = self.__load_tiempos_proceso()
         self.objetivo_inventario = self.__load_objetivo_inventario()
         self.inventario_inicial_puerto = self.__load_inventario_inicial_puerto()
-        self.llegadas_puerto = self.__load_llegadas_a_puerto()
+        self.inventario_totalizado_inicial_puerto = self.__load_inventario_totalizado_inicial_puerto()
+        self.llegadas_puerto = self.__load_llegadas_puerto()
+        self.llegadas_totalizadas_puerto = self.__load_llegadas_totalizadas_a_puerto()
+        self.costos_transporte = self.__load_costos_transporte()
+        self.costo_almacenamiento = self.__load_costos_almacenamiento()
+        
 
     def __load_file(self):
 
@@ -80,7 +86,163 @@ class Problema():
         # Intercompany
         self.intercompany_df = get_intercompany(bios_input_file=self.bios_input_file)
 
+    def __load_costos_almacenamiento(self):
+        df = self.costos_almacenamiento_df.set_index(['empresa', 'puerto', 'operador', 'ingrediente', 'importacion', 'fecha_corte'])
+
+        costo_almacenamiento = dict()
+        for i in self.importaciones:
+            costo_almacenamiento[i] = dict()
+            for periodo in self.periodos:
+                impo_index = tuple(list(i) + [periodo])
+                if impo_index in df.index:
+                    costo = df.loc[impo_index]['valor_kg']
+                else:
+                    costo = 0.0
+
+                if not periodo in costo_almacenamiento[i].keys():
+                    costo_almacenamiento[i][periodo] = dict()
+                costo_almacenamiento[i][periodo] = costo
+
+        return costo_almacenamiento
+
+    def __load_costos_transporte(self):
+        columns = ['Empresa', 'Puerto', 'Operador', 'Ingrediente', 'Importacion', 'valor_kg']
+        importaciones_df = self.cargas_despachables_df.groupby(columns)[[]].count().reset_index().copy()
+
+        df = importaciones_df.rename(columns={'Empresa': 'Empresa_Origen'}).copy()
+
+        # Cruzar con fechas de consumo
+        df = pd.merge(left=df,right=pd.DataFrame(self.periodos).rename(columns={0: 'Fecha'}), how='cross')
+
+        temp = self.fletes_df.copy()
+        columns = ['puerto', 'operador', 'ingrediente']
+        temp = temp.melt(id_vars=columns, value_vars=temp.drop(
+            columns=columns).columns, var_name='Planta', value_name='flete_kg')
+        temp['Empresa_Destino'] = temp['Planta'].map(self.empresas)
+        temp['Flete_Camion'] = self.cap_camion*temp['flete_kg']
+        temp.drop(columns=['flete_kg'], inplace=True)
+        temp.rename(columns={'puerto': 'Puerto', 'operador': 'Operador','ingrediente': 'Ingrediente'}, inplace=True)
+                
+        df = pd.merge(left=df, right=temp,
+              left_on=['Puerto', 'Operador', 'Ingrediente'],
+              right_on=['Puerto', 'Operador', 'Ingrediente'],
+              how='left')
+        
+        self.costo_portuario_directo_df = self.costo_portuario_directo_df.rename(columns={'operador': 'Operador',
+                                                                        'puerto': 'Puerto',
+                                                                        'ingrediente': 'Ingrediente',
+                                                                        'valor_kg': 'Directo'})
+        
+        # cruzar transitos con operacion portuaria de despacho directo
+        temp = pd.merge(left=self.transitos_a_puerto_df.reset_index().drop(columns=['Llegada', 'Camiones', 'valor_kg', 'Inventario']).rename(columns={'Empresa': 'Empresa_Origen'}),
+                right=self.costo_portuario_directo_df,
+                left_on=['Operador', 'Puerto', 'Ingrediente'],
+                right_on=['Operador', 'Puerto', 'Ingrediente'],
+                how='left')
+        
+        # temp['Fecha'] = temp['Fecha'].apply(lambda x: x.strftime("%Y-%m-%d"))
+        temp['Directo'] = self.cap_camion*temp['Directo']
+
+        # Anexar costos portuarios por despacho directo
+        df = pd.merge(left=df,
+                    right=temp,
+                    left_on=['Empresa_Origen', 'Puerto', 'Operador',
+                            'Ingrediente', 'Importacion', 'Fecha'],
+                    right_on=['Empresa_Origen', 'Puerto', 'Operador',
+                                'Ingrediente', 'Importacion', 'Fecha'],
+                    how='left')
+        
+        df['Directo'] = df['Directo'].fillna(0)
+
+        intercompany_df = self.intercompany_df.melt(id_vars='origen', 
+                                                    value_vars=list(set(self.empresas.values())), var_name='Empresa_Destino', 
+                                                    value_name='intercompany').rename(columns={'origen': 'Empresa_Origen'})
+        
+        # Intercompany
+        df = pd.merge(left=df,
+              right=intercompany_df,
+              left_on=['Empresa_Origen', 'Empresa_Destino'],
+              right_on=['Empresa_Origen', 'Empresa_Destino'],
+              how='left') 
+
+        # Calcular costo total despacho
+        df['CostoTotalCamion'] = df['Flete_Camion'] + df['Directo'] + \
+            (self.cap_camion*df['intercompany']*df['valor_kg'])  
+        
+        costo_transporte_df = df.copy()
+
+        costo_transporte_df.set_index(keys=['Empresa_Origen', 'Puerto', 'Operador',
+                              'Ingrediente', 'Importacion', 'Planta', 'Fecha'], inplace=True)
+        
+        costo_transporte = dict()
+        for i in self.importaciones:
+            costo_transporte[i] = dict()
+            for planta in self.plantas:
+                costo_transporte[i][planta] = dict()
+                for periodo in self.periodos:
+                    impo_index = tuple(list(i) + [planta, periodo])
+                    if impo_index in costo_transporte_df.index:
+                        costo = costo_transporte_df.loc[impo_index]['CostoTotalCamion']
+                        costo_transporte[i][planta][periodo] = costo
+        
+        return costo_transporte
+
     def __load_inventario_inicial_puerto(self):
+
+        inventario_inicial_puerto = dict()
+        inventario_inicial_puerto_df = self.cargas_despachables_df[self.cargas_despachables_df['Inventario'] > 0].copy()
+        inventario_inicial_puerto_df.set_index(keys=['Empresa', 'Puerto', 'Operador', 'Ingrediente', 'Importacion'], inplace=True)
+
+        
+        for i in self.importaciones:
+            if i in inventario_inicial_puerto_df.index:
+                cantidad = int(
+                    inventario_inicial_puerto_df.loc[i]['Inventario'])
+            else:
+                cantidad = 0
+            inventario_inicial_puerto[i] = cantidad
+
+        return inventario_inicial_puerto
+
+    def __load_lista_importaciones(self):
+        importaciones = list()
+        for i in range(self.cargas_despachables_df.shape[0]):
+            inventario = int(self.cargas_despachables_df.iloc[i]['Inventario'])
+            llegadas = int(self.cargas_despachables_df.iloc[i]['Llegada'])
+
+            if inventario > self.cap_camion or llegadas > self.cap_camion:
+
+                index = (self.cargas_despachables_df.iloc[i]['Empresa'],
+                        self.cargas_despachables_df.iloc[i]['Puerto'],
+                        self.cargas_despachables_df.iloc[i]['Operador'],
+                        self.cargas_despachables_df.iloc[i]['Ingrediente'],
+                        self.cargas_despachables_df.iloc[i]['Importacion'])
+
+                importaciones.append(index)
+
+        return list(set(importaciones))
+
+    def __load_llegadas_puerto(self):
+        
+        self.transitos_a_puerto_df = self.cargas_despachables_df[self.cargas_despachables_df['Llegada'] > 0].copy()
+        self.transitos_a_puerto_df.set_index(['Empresa', 'Puerto', 'Operador', 'Ingrediente', 'Importacion', 'Fecha'], inplace=True)
+
+        llegadas_puerto = dict()
+
+        for i in self.importaciones:
+            llegadas_puerto[i] = dict()
+            for periodo in self.periodos:
+                i2 = tuple(list(i) + [periodo])
+
+                if i2 in self.transitos_a_puerto_df.index:
+                    cantidad_puerto = int(self.transitos_a_puerto_df.loc[i2]['Llegada'])
+                else:
+                    cantidad_puerto = 0
+                llegadas_puerto[i][periodo] = cantidad_puerto
+
+        return llegadas_puerto
+
+    def __load_inventario_totalizado_inicial_puerto(self):
         # Transformar a camiones
         self.cargas_despachables_df['Camiones'] = self.cargas_despachables_df['Inventario'].apply(
             lambda x: int(x/self.cap_camion))
@@ -98,7 +260,7 @@ class Problema():
 
         return inventario_inicial_puerto
 
-    def __load_llegadas_a_puerto(self):
+    def __load_llegadas_totalizadas_a_puerto(self):
         # Transitos programados
         self.tto_puerto_df['Camiones'] = self.tto_puerto_df['Llegada'].apply(
             lambda x: int(x/self.cap_camion))

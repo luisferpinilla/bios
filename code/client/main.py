b98323f82fd09eede6b0d8279feafc30eca97e58
Luis Pinilla <luisferpinilla@gmail.com>
2024-08-08 21:42:12 -0500

    Ajuste de logs en cliente

--- code/client/main.py
+++ code/client/main.py
@@ -1,7 +1,9 @@
 from loader import Loader
 import logging
 
-logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s: %(message)s', 
+                    datefmt='%m/%d/%Y %I:%M:%S %p')
 
 
 input_file = file = "../../data/0_model_template_2506.xlsm"
@@ -12,3 +14,4 @@ problema = loader.problema
 
 fechas = loader.fechas
 
+data= problema['importaciones']['maiz']['baq']['trademar']['finca']['45842TONHILII']
